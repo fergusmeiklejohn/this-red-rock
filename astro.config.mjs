61d3612f651b70d08c81eb85cd40c78fe370eb67
Fergus Meiklejohn <fergusmeiklejohn@gmail.com>
2025-07-25 08:56:43 +0100

    when using the Cloudflare adapter, it sets output: 'server' mode which creates a Worker that can interfere with static asset serving. So we removed the cloudflare astro adaptor

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,17 +5,9 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
-import cloudflare from '@astrojs/cloudflare';
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://this-red-rock.com',
   integrations: [mdx(), sitemap(), tailwind(), react()],
-  adapter: cloudflare({
-    platformProxy: {
-      enabled: true
-    },
-
-    imageService: "passthrough"
-  }),
+  output: 'static'
 });
